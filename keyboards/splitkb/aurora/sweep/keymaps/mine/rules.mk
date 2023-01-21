a0eba4d4e631bd7bf17ca21940c87c6d7fb9e9a2
los-t <74127+los-t@users.noreply.github.com>
2023-01-21 21:19:41 +0100

    SWEEP.MINE: reduce firmware size
    by disabling the features I either don't physically have or don't use.

--- keyboards/splitkb/aurora/sweep/keymaps/mine/rules.mk
+++ keyboards/splitkb/aurora/sweep/keymaps/mine/rules.mk
@@ -1,3 +1,10 @@
 CAPS_WORD_ENABLE = yes
 NKRO_ENABLE = yes
 COMBO_ENABLE = yes
+
+# Override Aurora Sweep defaults that I don't use
+MOUSEKEY_ENABLE = no
+ENCODER_ENABLE = no
+RGB_MATRIX_ENABLE = no
+OLED_ENABLE = no
+BOOTMAGIC_ENABLE = no
